9f5e820793710b55b02eb990041709f8957dd99c
刘智超 <zhichaoleo@163.com>
2017-03-20 11:16:49 +0800

    first submit

---
+++ CPP_primer/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+#include<windows.h>
+#include<vector>
+#include<cstdlib>
+#include<algorithm>
+#include<cstring>
+#include<iterator>
+#include<cstddef>
+#include<exception>
+
+using namespace std;
+int a[10];
+int main()
+{
+	int a;
+	char b;
+	float c;
+	double d;
+	int *p;
+	
+		cout << sizeof(a) << sizeof(b) << sizeof(c)<< sizeof(d)<<sizeof(p)<< endl;
+	
+	
+
+	system("pause");
+	return 0;
+
+}
